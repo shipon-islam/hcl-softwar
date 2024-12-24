d595f1252f7350e9d54b8c078799fb89dfd507cf
Shipon Islam <78357259+shipon-islam@users.noreply.github.com>
2024-12-24 22:10:14 +0600

    Update Notification.jsx

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,10 +1,10 @@
 import NotificationList from "../components/NotificationList";
-
+import { notificationApi } from "@/constants";
 export default function Notification() {
   return (
     <div className="layout-padding bg-white">
       <h1 className="font-bold text-xl pt-4">Notifications</h1>
-      <NotificationList notificationApi={[]} />
+      <NotificationList notificationApi={notificationApi} />
     </div>
   );
 }
